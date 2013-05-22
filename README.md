f874cb5737ab6b72b74c265cb0172f0857f994ef
Brenton Simpson <git@appsforartists.com>
2013-05-22 11:04:57 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+ This is me trying to remember all the stuff I did to turn my Pixel into a web dev machine.  Hopefully, I can save you a couple days of your life experimenting with all sorts of Linuxy stuff before you can be productive.
+
+TODO:
+
+ - Add AppEngine
+ - Add Sublime Text (if license allows)
+ - Add PackageControl
+ - Remove everything I didn't change in Soda Dark.sublime-theme.  Most retina Sublime themes seem to be based on Soda, so hopefully I can create a scaffold that will teach Sublime where to find Retina assets for any Sublime theme.
+
+ Better docs to come.  I haven't tested these scripts yet, so be careful!
---
+++ run_from_crosh.sh
@@ -0,0 +1,19 @@
+#~/bin/bash
+
+
+if [! -f /home/chronos/user/Downloads/crouton ]; then
+    wget -O /home/chronos/user/Downloads/crouton http://goo.gl/fd3zc
+fi
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Please run \"sudo su -\" first, then try again"
+    exit 1
+fi
+
+sh -e ~/Downloads/crouton -d -f ~/Downloads/crouton_base.tar.bz2
+sh -e ~/Downloads/crouton -f ~/Downloads/crouton_base.tar.bz2 -r raring -t touch -n dev
+
+# copy chromeos assets into chroot
+cp -r /usr/share/cursors/xorg-x11/chromeos /usr/local/chroots/dev/usr/share/icons/
+cp -r /usr/share/icons/hicolor /usr/local/chroots/dev/usr/share/icons/
+cp -r /usr/share/fonts /usr/local/chroots/dev/usr/share/
