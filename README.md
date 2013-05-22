5b45e3f9a8650e902c951c2a8a0d58510e61b5de
Brenton Simpson <git@appsforartists.com>
2013-05-22 12:00:56 -0700

    Automatically downloading and installing AppEngine and Sublime

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 TODO:
 
- - Add AppEngine
- - Add Sublime Text (if license allows)
- - Add PackageControl
  - Remove everything I didn't change in Soda Dark.sublime-theme.  Most retina Sublime themes seem to be based on Soda, so hopefully I can create a scaffold that will teach Sublime where to find Retina assets for any Sublime theme.
+ 
+ - Test the .sh scripts
 
- Better docs to come.  I haven't tested these scripts yet, so be careful!
+ - Combine both scripts
+
+ - Documentation
--- run_from_chroot.sh
+++ run_from_chroot.sh
@@ -38,3 +38,23 @@ cp -r ./root/home/user/* ~/
 
 echo "# let dropbox run" >> /etc/sysctl.conf
 echo "fs.inotify.max_user_watches=100000" >> /etc/sysctl.conf
+
+mkdir ~/Applications
+mkdir ~/bin
+
+# install Sublime Text
+wget -O "~/Downloads/Sublime Text 2.0.1.tar.bz2" http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20x64.tar.bz2
+tar -xvjf "~/Downloads/Sublime Text 2.0.1.tar.bz2" ~/Applications/
+ln -s "~/Applications/Sublime Text 2/sublime_text" ~/bin/subl
+wget -O "~/Downloads/Package Control.zip" https://sublime.wbond.net/Package%20Control.sublime-package
+python -m zipfile -e "~/Downloads/Package Control.zip" "~/.config/sublime-text-2/Packages/Package Control"
+git clone https://github.com/buymeasoda/soda-theme/ "~/.config/sublime-text-2/Packages/Theme - Soda"
+
+# install AppEngine
+wget -O ~/Downloads/cloudsdk-0.9.2-linux-python.zip https://dl.google.com/dl/cloudsdk/cloudsdk-0.9.2-linux-python.zip
+python -m zipfile -e ~/Downloads/cloudsdk-0.9.2-linux-python.zip ~/Applications/cloud_sdk
+mv ~/Applications/cloud_sdk/cloudsdk-0.9.2/* ~/Applications/cloud_sdk
+rmdir ~/Applications/cloud_sdk/cloudsdk-0.9.2
+
+# Add ~/bin and AppEngine to path
+echo "export PATH=$PATH:~/bin/:~/Applications/cloud_sdk/bin/:~/bin/:~/Applications/cloud_sdk/platform/google_appengine/" >> ~/.bashrc
