dfdb2ebba6ad798a125a7021bcc21debaaccc117
Brenton <git@appsforartists.com>
2013-05-23 00:42:00 -0700

    Splitting up chroot script into user and root scripts

---
+++ run_from_chroot_as_root.sh
@@ -0,0 +1,43 @@
+#~/bin/bash
+
+if [ -d /home/chronos/ ]; then
+    echo "You must run this script from inside your chroot!"
+    echo "Try running:"
+    echo "    cp ./init.sh /usr/share/chroot/dev/"
+    echo "    sudo enter-chroot -n dev"
+    echo "    sudo /init.sh"
+fi
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Please run \"sudo su -\" first, then try again"
+    exit 1
+fi
+
+# install add-apt-repository
+apt-get install python-software-properties
+
+# fix occasional dbus not found errors
+apt-get install dbus-x11
+
+# install window manager
+apt-get install openbox
+apt-get install lxappearance
+apt-get install python-xdg
+
+# install git
+apt-get install git
+apt-get install keychain
+
+# pick a theme that matches sublime
+add-apt-repository ppa:webupd8team/themes
+apt-get install mediterraneannight-gtk-theme
+
+# install a terminal with resizable fonts
+add-apt-repository ppa:elementaryos/daily
+apt-get install pantheon-terminal
+
+cp ./root/etc/* /etc/
+
+echo "# let dropbox run" >> /etc/sysctl.conf
+echo "fs.inotify.max_user_watches=100000" >> /etc/sysctl.conf
+
---
+++ run_from_chroot_as_user.sh
@@ -0,0 +1,36 @@
+#~/bin/bash
+
+if [ -d /home/chronos/ ]; then
+    echo "You must run this script from inside your chroot!"
+    echo "Try running:"
+    echo "    cp ./init.sh /usr/share/chroot/dev/"
+    echo "    sudo enter-chroot -n dev"
+    echo "    /init.sh"
+fi
+
+if [[ $EUID -eq 0 ]]; then
+    echo "This script should be run from your regular user account (not root)."
+    exit 1
+fi
+
+rsync -a ./root/home/user/ ~/
+
+mkdir ~/Applications
+mkdir ~/bin
+
+# install Sublime Text
+wget -O ~/Downloads/sublime_text_2.0.1.tar.bz2 http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20x64.tar.bz2
+tar -xvjf ~/Downloads/sublime_text_2.0.1.tar.bz2 -C ~/Applications/
+ln -s "~/Applications/Sublime Text 2/sublime_text" ~/bin/subl
+wget -O ~/Downloads/package_control.zip https://sublime.wbond.net/Package%20Control.sublime-package
+python -m zipfile -e "~/Downloads/package_control.zip" "~/.config/sublime-text-2/Packages/Package Control"
+git clone https://github.com/buymeasoda/soda-theme/ ~/.config/sublime-text-2/Packages/Theme\ -\ Soda
+
+# install AppEngine
+wget -O ~/Downloads/cloudsdk-0.9.2-linux-python.zip https://dl.google.com/dl/cloudsdk/cloudsdk-0.9.2-linux-python.zip
+python -m zipfile -e ~/Downloads/cloudsdk-0.9.2-linux-python.zip ~/Applications/cloud_sdk
+mv ~/Applications/cloud_sdk/cloudsdk-0.9.2/* ~/Applications/cloud_sdk
+rmdir ~/Applications/cloud_sdk/cloudsdk-0.9.2
+
+# Add ~/bin and AppEngine to path
+echo "export PATH=$PATH:~/bin/:~/Applications/cloud_sdk/bin/:~/bin/:~/Applications/cloud_sdk/platform/google_appengine/" >> ~/.bashrc
