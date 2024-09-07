a198e806a6a9f55476cdf3b4b973d03c60e64f01
Brenton Simpson <appsforartists@google.com>
2024-09-07 18:33:58 -0400

    remove hardcoded timezone

--- hosts/go/configuration.nix
+++ hosts/go/configuration.nix
@@ -55,9 +55,6 @@
     wifi.powersave = false;
   };
 
-  # Set your time zone.
-  time.timeZone = "America/Los_Angeles";
-
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
