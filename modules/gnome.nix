4fbcee7accc1b0d84dd3ef0c6b8c57721ce9a6d3
Brenton Simpson <appsforartists@google.com>
2024-11-21 23:18:02 -0800

    patch names that changed in 24.11

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -15,14 +15,14 @@
     jovian.steam.desktopSession = "gnome";
     services.xserver.displayManager.gdm.enable = lib.mkForce (!config.jovian.steam.autoStart);
 
-    services.xserver.displayManager.autoLogin = {
+    services.displayManager.autoLogin = {
       enable = true;
       user = config.mainUser.userName;
     };
 
     # remove all the GNOME default apps, and explicitly pick the ones that might be useful
     services.gnome.core-utilities.enable = false;
-    environment.systemPackages = with pkgs; with pkgs.gnome; [
+    environment.systemPackages = with pkgs; [
       baobab # disk space analyzer
       gnome-console # terminal
       gnome-font-viewer
