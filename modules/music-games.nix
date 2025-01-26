6ae385e772d2c01f9dc1275a10f3e9e1e7b071f5
Brenton Simpson <git@appsforartists.com>
2025-01-25 16:24:52 -0800

    mount .clonehero from SD card

--- modules/music-games.nix
+++ modules/music-games.nix
@@ -11,5 +11,15 @@
       clonehero
       onyx
     ];
+
+    fileSystems."/home/${config.mainUser.userName}/.clonehero" = {
+      device = "/run/media/mmcblk0p1/Clone Hero";
+      fsType = "none";
+      options = [
+        "bind"
+        "x-systemd.automount"
+        "noauto"
+      ];
+    };
   };
 }
