8895e120a95d48ed8df816ca1a75d3a0af46fb56
Brenton Simpson <git@appsforartists.com>
2025-01-26 18:19:20 -0800

    add yarg

--- modules/music-games.nix
+++ modules/music-games.nix
@@ -10,10 +10,21 @@
     environment.systemPackages = with pkgs; [
       clonehero
       onyx
+      yarg
     ];
 
     fileSystems."/home/${config.mainUser.userName}/.clonehero" = {
       device = "/run/media/mmcblk0p1/Clone Hero";
+      fsType = "none";
+      options = [
+        "bind"
+        "x-systemd.automount"
+        "noauto"
+      ];
+    };
+
+    fileSystems."/home/${config.mainUser.userName}/Yarg" = {
+      device = "/run/media/mmcblk0p1/Yarg";
       fsType = "none";
       options = [
         "bind"
