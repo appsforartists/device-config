8cb6b221253a6ea91a57484e4dcb0e87521ac23a
Brenton Simpson <appsforartists@google.com>
2025-11-11 10:26:52 -0500

    removing redundant desktop entry for sublime

--- modules/sublime/default.nix
+++ modules/sublime/default.nix
@@ -40,14 +40,5 @@
     programs.bash.bashrcExtra = ''
       export EDITOR="subl -w"
     '';
-
-    xdg.desktopEntries.sublime-text = {
-      name = "Sublime Text";
-      exec = "${pkgs.sublime4}/bin/subl %F";
-      icon = "sublime-text";
-      type = "Application";
-      categories = ["Development" "TextEditor"];
-      mimeType = ["text/plain"];
-    };
   };
 }
