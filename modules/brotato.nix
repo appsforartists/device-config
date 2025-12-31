d22f4c164489a87e64156e1bfe80ff035d271d04
Brenton Simpson <appsforartists@google.com>
2025-12-30 21:40:14 -0800

    give brotato an icon

--- modules/brotato.nix
+++ modules/brotato.nix
@@ -6,12 +6,18 @@
 pkgs.stdenv.mkDerivation (finalAttrs: {
   name = "brotato";
   src = /nix/store/grynblqk7023xsv6sgfr7hc8vy76fqjh-Brotato.pck;
+  srcIcon = pkgs.fetchurl {
+    name = "brotato.icns";
+    url = "https://shared.fastly.steamstatic.com/community_assets/images/apps/1942280/c096e6b30bcb9749183fe5d1b78f77de7ae89383.icns";
+    hash = "sha256-6ZZ1kqdOjqwIByrX1Bqrhy2yMaShlsbsEhuDBTK77gw=";
+  };
 
   unpackPhase = ":";
 
   nativeBuildInputs = [
     pkgs.makeWrapper
     pkgs.godot3
+    pkgs.libicns
     pkgs.copyDesktopItems
   ];
 
@@ -22,6 +28,11 @@ pkgs.stdenv.mkDerivation (finalAttrs: {
     makeWrapper ${lib.getExe' pkgs.godot3 "godot3"} $out/bin/brotato \
       --add-flags "--main-pack $src"
 
+    cp $srcIcon brotato.icns
+    icns2png -x brotato.icns
+    mkdir -p $out/share/icons/hicolor/512x512/apps
+    install -Dm644 brotato_512x512x32.png $out/share/icons/hicolor/512x512/apps/brotato.png
+
     runHook postInstall
   '';
 
@@ -29,7 +40,7 @@ pkgs.stdenv.mkDerivation (finalAttrs: {
     (pkgs.makeDesktopItem {
       name = "brotato";
       desktopName = "Brotato";
-      exec = "brotato";
+      exec = "${placeholder "out"}/bin/brotato";
       icon = "brotato";
       comment = finalAttrs.meta.description;
       categories = ["Game" "ActionGame" "Shooter"];
