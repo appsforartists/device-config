ca5589f0d8007a01cfaf76e7df785a9f7f59917c
Brenton Simpson <appsforartists@google.com>
2026-01-02 13:19:58 -0800

    use flakes

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Home Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    homeConfigurations."deck" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [./home.nix];
+    };
+  };
+}
