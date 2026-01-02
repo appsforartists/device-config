4cb75200a7860f63c711e5c69cec9b6387f40d5c
Brenton Simpson <appsforartists@google.com>
2026-01-02 13:41:30 -0800

    add git aliases

--- modules/shell.nix
+++ modules/shell.nix
@@ -17,12 +17,32 @@
          . "${config.home.homeDirectory}/.nix-profile/etc/profile.d/nix-daemon.sh"
         fi
       '';
-
       profileExtra = ''
         if [ -f "${config.home.homeDirectory}/.nix-profile/etc/profile.d/nix-daemon.sh" ]; then
          . "${config.home.homeDirectory}/.nix-profile/etc/profile.d/nix-daemon.sh"
         fi
       '';
+
+      shellAliases = {
+        g = "git";
+        gcp = "git cherry-pick";
+        grod = "git rebase origin/develop";
+        gri = "git rebase -i";
+        griod = "git rebase -i origin/develop";
+        gmff = "git merge --ff-only";
+        grc = "git rebase --continue";
+        grs = "git rebase --skip";
+        gcl = "git clone";
+        gco = "git checkout";
+        glo = "git log --oneline --abbrev-commit --graph --decorate --color";
+        gsb = "git branch --verbose";
+        gst = "git status";
+        pscb = "git push";
+        yrs = "yarn run start";
+        yrbw = "yarn run build --watch";
+        yrt = "yarn run test";
+        sbrc = "subl ~/.bashrc";
+      };
     };
   };
 }
