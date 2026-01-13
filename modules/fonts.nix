24fe6f750279fa02e99ea8b58f1f5076f1fbcf63
Brenton Simpson <appsforartists@google.com>
2026-01-12 21:24:09 -0500

    install fonts on mac after hms

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -5,13 +5,23 @@
   pkgs,
   ...
 }: {
-  config = {
-    fonts.fontconfig.enable = true;
-    home = {
-      packages = with pkgs; [
-        inconsolata
-        noto-fonts-color-emoji
-      ];
-    };
-  };
+  config = lib.mkMerge [
+    {
+      fonts.fontconfig.enable = true;
+      home = {
+        packages = with pkgs; [
+          inconsolata
+          noto-fonts-color-emoji
+        ];
+      };
+    }
+
+    (lib.mkIf pkgs.stdenv.isDarwin {
+      home.activation.installFonts = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        if [ -d "${config.home.path}/share/fonts" ]; then
+          $DRY_RUN_CMD find -L "${config.home.path}/share/fonts" -type f \( -name '*.ttf' -o -name '*.otf' \) -exec ln -sf {} "$HOME/Library/Fonts/" \;
+        fi
+      '';
+    })
+  ];
 }
