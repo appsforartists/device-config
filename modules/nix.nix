84ac34fb2f21e698322284170363d2a902eec2c9
Brenton Simpson <appsforartists@google.com>
2026-01-15 00:01:31 -0500

    add determinate nix substitutors to nix.nix

--- modules/nix.nix
+++ modules/nix.nix
@@ -34,7 +34,10 @@
       bash-prompt-prefix = (nix:%n)
       extra-nix-path = nixpkgs=flake:nixpkgs
       substituters = https://cache.nixos.org
+      extra-substituters = https://install.determinate.systems
+      extra-trusted-substituters = https://cache.flakehub.com https://install.determinate.systems
       trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
+      extra-trusted-public-keys = cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM= cache.flakehub.com-4:Asi8qIv291s0aYLyH6IOnr5Kf6+OF14WVjkE6t3xMio= cache.flakehub.com-5:zB96CRlL7tiPtzA9/WKyPkp3A2vqxqgdgyTVNGShPDU= cache.flakehub.com-6:W4EGFwAGgBj3he7c5fNh9NkOXw0PUVaxygCVKeuvaqU= cache.flakehub.com-7:mvxJ2DZVHn/kRxlIaxYNMuDG1OvMckZu32um1TadOR8= cache.flakehub.com-8:moO+OVS0mnTjBTcOUh2kYLQEd59ExzyoW1QgQ8XAARQ= cache.flakehub.com-9:wChaSeTI6TeCuV/Sg2513ZIM9i0qJaYsF+lZCXg0J6o= cache.flakehub.com-10:2GqeNlIp6AKp4EF2MVbE1kBOp9iBSyo0UPR9KoR0o1Y=
     '';
     # Home Manager owns ~ and the binaries inside of it.  On a single-user
     # install, that includes `nix` itself.  On a Determinate install, nix is on
