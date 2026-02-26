e2c665f8bd4f79eacbff08c8e60bb41135d829b9
Brenton Simpson <appsforartists@google.com>
2026-02-26 17:07:35 -0500

    set terminalBackgroundColor to purple for corp linux

--- environments/corp.nix
+++ environments/corp.nix
@@ -1,21 +1,30 @@
 {
   config,
   inputs,
+  lib,
   pkgs,
   secrets,
   ...
 }: {
-  my.system = {
-    environment = "corp";
-    hasDeterminate = true;
-  };
-
   imports = [
     ./common.nix
   ];
 
-  home.sessionVariables = with secrets; {
-    GOOGLE_CLOUD_PROJECT_ID = googleCloudProjectID;
-    GEMINI_API_KEY = geminiAPIKey;
+  config = lib.my.byPlatform {
+    common = {
+      my.system = {
+        environment = "corp";
+        hasDeterminate = true;
+      };
+
+      home.sessionVariables = with secrets; {
+        GOOGLE_CLOUD_PROJECT_ID = googleCloudProjectID;
+        GEMINI_API_KEY = geminiAPIKey;
+      };
+    };
+
+    linux = {
+      my.system.terminalBackgroundColor = "#5D007F";
+    };
   };
 }
